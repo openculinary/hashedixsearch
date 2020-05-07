d468155fb8a922b0eb137d212afe69149ea8d2d0
James Addison <james@reciperadar.com>
2020-05-07 14:20:07 +0100

    Retain multi-whitespace tokens in SynonymAnalyzer.analyze_token

--- hashedixsearch/search.py
+++ hashedixsearch/search.py
@@ -52,7 +52,7 @@ class SynonymAnalyzer(WhitespacePunctuationTokenAnalyzer):
 
     def analyze_token(self, token):
         synonym = self.synonyms.get(token) or token
-        for token in re.split("( )", synonym):
+        for token in re.split(r"(\w+)", synonym):
             yield token
 
 
