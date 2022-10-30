01e68cf55166d53bc1513e46ba3c98c9bc34cd56
James Addison <55152140+jayaddison@users.noreply.github.com>
2022-10-30 16:07:58 +0000

    Refactor: cleanup stdlib imports (#33)

--- hashedixsearch/_utils.py
+++ hashedixsearch/_utils.py
@@ -1,16 +1,15 @@
 from string import punctuation
-from xml.etree.ElementTree import Element, tostring
 
 
 def _is_separator(token):
     if token.isspace():
         return True
-    if token.strip(punctuation) == str():
+    if not token.strip(punctuation):
         return True
     return False
 
 
-def _render_match(cstring, attributes):
-    element = Element("mark", attributes or {})
-    element.text = cstring.getvalue()
-    return tostring(element, encoding="unicode")
+def _render_match(match, attributes):
+    attributes = attributes or {}
+    attribs = "".join(f' {key}="{value}"' for key, value in attributes.items())
+    return f"<mark{attribs}>{match}</mark>"
